db6b1cf9cdfb3ccfa50faf71259662976aedb717
Emmanuel Allan <90669430+EmmanuelAllanMJ@users.noreply.github.com>
2022-10-23 16:46:36 +0530

    Added base64 encryption logic

--- popup.js
+++ popup.js
@@ -13,4 +13,11 @@ $(function () {
       });
     });
   });
+  $("#Base64").click(function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        todo: "base64",
+      });
+    });
+  });
 });
