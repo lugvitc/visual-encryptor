a95a8f60602d1fe883bae15f48842ee960a86f92
Emmanuel Allan <90669430+EmmanuelAllanMJ@users.noreply.github.com>
2022-10-23 15:16:15 +0530

    Added reset functionallity

--- content.js
+++ content.js
@@ -1,11 +1,24 @@
 chrome.runtime.sendMessage({ todo: "showPageAction" });
+var realContent = [];
+console.log(realContent);
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.todo == "ceasarEncrypt") {
     var pArray = document.getElementsByTagName("p");
     for (var i = 0; i < pArray.length; i++) {
       var text = document.getElementsByTagName("p")[i].innerText;
+      if (realContent.length < pArray.length) {
+        realContent.push(text);
+      }
       var encryptText = ceaserencrypt(text, 5);
       document.getElementsByTagName("p")[i].innerText = encryptText;
+    }
+    console.log(realContent);
+  }
+  if (request.todo == "reset") {
+    console.log(realContent);
+    for (var i = 0; i < realContent.length; i++) {
+      var text = realContent[i];
+      document.getElementsByTagName("p")[i].innerText = text;
     }
   }
 });
